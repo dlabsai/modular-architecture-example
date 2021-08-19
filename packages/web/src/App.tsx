f9990bfbe14f8f0a8b2dd455a91a737109c17394
Krzysztof Miśtal <krzysztof.mistal@dlabs.ai>
2021-08-19 15:17:14 +0200

    Add mobile package Bootstrap all the parts together

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getEnumValues } from '@wbt/core/dist/modules/TestModule/services/TestService';
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{''}</p>
+        <p>{getEnumValues().a}</p>
       </header>
     </div>
   );
